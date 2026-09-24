226db3df087ef27f571e2b48a7d566b972ffeb42
Tabele Joseph Osogwereromo <158265015+reto-329@users.noreply.github.com>
2026-09-24 16:24:28 +0100

    Fix static file serving path in server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Simple CORS middleware for this project
 app.use((req, res, next) => {
